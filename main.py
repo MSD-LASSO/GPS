23b69ee8d8d09cb56da47bf2e6bce9fcc17d60f3
Luca <ils5482@rit.edu>
2020-02-17 19:19:41 -0500

    Fixed crucial bug that would prevent GPS from working on cold start of Pi

--- main.py
+++ main.py
@@ -6,15 +6,21 @@ import math
 x=L76X.L76X()
 x.L76X_Set_Baudrate(9600)
 x.L76X_Send_Command(x.SET_NMEA_BAUDRATE_115200)
-time.sleep(5)
+time.sleep(2)
+x.L76X_Send_Command(x.SET_NMEA_BAUDRATE_115200)
+time.sleep(2)
+x.L76X_Send_Command(x.SET_NMEA_BAUDRATE_115200)
+time.sleep(2)
 x.L76X_Set_Baudrate(115200)
+x.L76X_Send_Command(x.SET_NMEA_BAUDRATE_115200)
+time.sleep(2)
 
 x.L76X_Send_Command(x.SET_POS_FIX_100MS);
 
 #Set output message
 x.L76X_Send_Command(x.SET_NMEA_OUTPUT);
 
-x.L76X_Exit_BackupMode();
+#x.L76X_Exit_BackupMode();
 
 f=open("Coordinates.txt", "w+")
 
