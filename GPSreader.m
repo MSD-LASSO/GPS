1a34b2a2aad72a1152b805f809badd1816ded634
Anthony I <awi7573@rit.edu>
2020-02-26 17:23:39 -0500

    NEw data files

--- GPSreader.m
+++ GPSreader.m
@@ -1,5 +1,8 @@
 close all
-Data=dlmread('Coordinates2.txt','');
+Data=dlmread('LucasBackYard.txt','');
+
+
+Colors=[0,0.447000000000000,0.741000000000000;0.850000000000000,0.325000000000000,0.0980000000000000;0.929000000000000,0.694000000000000,0.125000000000000;0.494000000000000,0.184000000000000,0.556000000000000;0.466000000000000,0.674000000000000,0.188000000000000;0.301000000000000,0.745000000000000,0.933000000000000;0.635000000000000,0.0780000000000000,0.184000000000000];
 
 Data=Data(Data(:,1)>0,:);
 
@@ -12,6 +15,7 @@ zlabel('Altitude (m)')
 title('GPS Data')
 axis square
 means=mean(Data);
+triangleMeans=means;
 medians=median(Data);
 stdDev=std(Data);
 figure()
@@ -33,13 +37,13 @@ Labs={'Degrees','Degrees','Meters'};
 figure()
 for i=1:3
     subplot(1,3,i)
-    histogram(Data(:,i),'color','blue','Normalization','probability')
+    histogram(Data(:,i),'Normalization','probability')
     xlabel(Labs{i})
     ylabel('Relative Frequency')
     title(Tits{i})
 end
 
-GraphSaver({'png','fig'},'plots/GPS',0,1)
+GraphSaver({'png','fig'},'plots/LucasBackYard',0,1)
 
 video=VideoWriter('GPSvid');
 open(video)
@@ -50,15 +54,19 @@ El=[45,0,0,90];
 xv=[h1.Children().XLim];
 yv=[h1.Children().YLim];
 zv=[h1.Children().ZLim];
+means=[43.109789,-77.410037,533*0.3048];
 meanToGraph=[xv ones(1,4)*means(1); means(2) means(2) yv means(2) means(2); ones(1,4)*means(3) zv];
 
 for i=1:4
 subplot(2,2,i)
 % plot3(43.084224,-77.677684,169.164) %from 525 + 30 ft.
-plot3(meanToGraph(1,1:2),meanToGraph(2,1:2),meanToGraph(3,1:2),'linewidth',2,'color','red');
+plot3(meanToGraph(1,1:2),meanToGraph(2,1:2),meanToGraph(3,1:2),'linewidth',0.5,'color','red');
 hold on
-plot3(meanToGraph(1,3:4),meanToGraph(2,3:4),meanToGraph(3,3:4),'linewidth',2,'color','red');
-plot3(meanToGraph(1,5:6),meanToGraph(2,5:6),meanToGraph(3,5:6),'linewidth',2,'color','red');
+plot3(meanToGraph(1,3:4),meanToGraph(2,3:4),meanToGraph(3,3:4),'linewidth',0.5,'color','red');
+plot3(meanToGraph(1,5:6),meanToGraph(2,5:6),meanToGraph(3,5:6),'linewidth',0.5,'color','red');
+
+plot3(triangleMeans(1),triangleMeans(2),triangleMeans(3),'v','linewidth',2);
+
 % a(i)=plot3(means(1),means(2),means(3),'s','linewidth',2,'MarkerFaceColor','red','color','red');
 xlim(xv)
 ylim(yv)
@@ -72,23 +80,31 @@ axis square
 
 end
 
-n=2000;
+n=10000;
+
+p=0;
 for i=1:n:length(Data)
     for j=1:4
         subplot(2,2,j)
+        p=p+1;
+        if p>size(Colors,1)
+            p=1;
+        end
         if i+n<length(Data)
-            b=plot3(Data(i:i+n,1),Data(i:i+n,2),Data(i:i+n,3),'.','color','black','MarkerSize',0.005);
+            b=plot3(Data(i:i+n,1),Data(i:i+n,2),Data(i:i+n,3),'.','color',Colors(p,:),'MarkerSize',10);
         else
-            b=plot3(Data(i:end,1),Data(i:end,2),Data(i:end,3),'.','color','black','MarkerSize',0.005);
+            b=plot3(Data(i:end,1),Data(i:end,2),Data(i:end,3),'.','color',Colors(p,:),'MarkerSize',10);
         end            
         uistack(b,'bottom') %you can also do uistack(b,'bottom')
     end
     
     frame=getframe(gcf);
     writeVideo(video,frame)
-    %     drawnow
+% %         drawnow
     pause(0.00000000001)
-end
+end]
+
+error=means-triangleMeans
 
 close(video)
 
