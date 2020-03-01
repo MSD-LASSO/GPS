c030c28ad739f0a6ff63beffb08cc02d319cea4e
Anthony I <awi7573@rit.edu>
2020-03-01 15:49:07 -0500

    Schedule updates

--- Unification_Attempt/unified.py
+++ Unification_Attempt/unified.py
@@ -42,6 +42,7 @@ with open('/home/pi/GPS/'+fileName) as f:
     reader=csv.reader(f,delimiter='\t')
     for row in reader:
         Str=str.split(row[0],"    ")
+        print("Raw String: "+Str[0])
         h1=datetime.strptime(Str[0],"%Y-%m-%dT%H:%M:%S.%f")
         Date.append(h1)
         Doppler.append(float(Str[1]))
