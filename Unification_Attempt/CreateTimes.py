103d097e095c1fad79c35d0b36ed9bb309aeade2
Anthony I <awi7573@rit.edu>
2020-03-06 15:04:57 -0500

    Updates to the scheduler and createTimes

--- Unification_Attempt/CreateTimes.py
+++ Unification_Attempt/CreateTimes.py
@@ -6,18 +6,40 @@ from optparse import OptionParser
 
 
 
-def createText(startTime,interval,numEntries):
+def createText(startTime,delay,numEntries,oscillate,sampleLength):
     f = open("./InputTimes.txt", "w+")
-
-    f.write(startTime+'     '+'000.0000000000'+'\n')
-
+    
+    referenceLength=5
+    #Initial Startup
     dateObj=dt.strptime(startTime, "%Y-%m-%dT%H:%M:%S.%f")
+
     for i in range(numEntries):
-        dateObj+= bigDT.timedelta(seconds=interval)
-        f.write(str(dateObj.date())+'T'+str(dateObj.time()) +'     '+'000.0000000000'+ '\n')
+        if oscillate==1:
+            writeToText(f, dateObj, 0, referenceLength)
+            dateObj += bigDT.timedelta(seconds=referenceLength + delay)
+            writeToText(f, dateObj, 437, sampleLength)
+        else:
+            writeToText(f, dateObj, 0, sampleLength)
+        dateObj += bigDT.timedelta(seconds=sampleLength + delay)
+
+
+    # f.write(startTime+'     '+'000.0000000000'+'     '+str(sampleLength)+'\n')
+    # 
+    # dateObj=dt.strptime(startTime, "%Y-%m-%dT%H:%M:%S.%f")
+    # for i in range(numEntries):
+    #     if oscillate:
+    #         dateObj += bigDT.timedelta(seconds=sampleLength+)
+    #         f.write(str(dateObj.date()) + 'T' + str(dateObj.time()) + '     ' + '000.0000000000' + '\n')
+    #     #offset date by the delay.
+    #     dateObj+= bigDT.timedelta(seconds=delay)
+    #     f.write(str(dateObj.date())+'T'+str(dateObj.time()) +'     '+'000.0000000000'+ '\n')
 
 
     # f.write('%f' % x.Lat + ' %f' % x.Lon + ' %f' % x.Alt + ' %0.f:' % x.Time_H + '%0.f:' % x.Time_M + '%.2f' % x.Time_S + '\n')
+
+def writeToText(f,dateObj,freq,sampleLength):
+    # f.write(str(dateObj.date()) + 'T' + str(dateObj.time()) + '     ' + format(freq,'.12f') + '     ' + format(sampleLength,'.12f') + '\n')
+    f.write(str(dateObj.date()) + 'T' + str(dateObj.time()) + '     ' + '{: >12f}'.format(freq) + '     ' + '{: >12f}'.format(sampleLength) + '\n')
 
 
 def argument_parser():
@@ -26,11 +48,17 @@ def argument_parser():
         "", "--startTime", dest="startTime", type="string", default=str(dt.now().date())+'T'+str((dt.now()+bigDT.timedelta(hours=5,minutes=5)).time()),
         help="Set DateTime in form 'YY-MM-DDTHH-MM-SS.fff' [default=%default]")
     parser.add_option(
-        "", "--interval", dest="interval", type="float", default=20,
-        help="Set interval between scheduled times [default=%default]")
+        "", "--delay", dest="delay", type="float", default=20,
+        help="Set delay between scheduled times [default=%default]")
     parser.add_option(
         "", "--numEntries", dest="numEntries", type="int", default=4,
         help="Number of entries in the text file [default=%default]")
+    parser.add_option(
+        "", "--oscillate", dest="oscillate", type="int", default=0,
+        help="Set to true to add a reference call 5 seconds before and after the satellite call [default=%default]")
+    parser.add_option(
+        "", "--sampleLength", dest="sampleLength", type="float", default=5,
+        help="Length of recording. Number of Samples will be Length * Sample Rate of 20,000,000 [default=%default]")
     return parser
 
 
@@ -38,7 +66,7 @@ def main(options=None):
     if options is None:
         options, _ = argument_parser().parse_args()
 
-    createText(options.startTime,options.interval,options.numEntries)
+    createText(options.startTime,options.delay,options.numEntries,options.oscillate,options.sampleLength)
 
 
 if __name__ == '__main__':
--- Unification_Attempt/unified.py
+++ Unification_Attempt/unified.py
@@ -4,6 +4,35 @@ from datetime import datetime, date
 import math
 import os
 
+def getCurrentTime(GPShandler):
+    try:
+        GPShandler.L76X_Get()
+        status=0
+    except:
+        os.system("sudo echo Failed To Get GPS >> /home/pi/Documents/debugger.txt 2>&1")
+        status=1
+    # print('\n')
+
+    #Get the current time to the best of our ability.
+    if (status==0):
+        GPS=True
+        # 2020-3-1
+        # 20:17:2.5
+        Str=x.Date+'T'+x.Time
+        # Str='2020-3-1'+'T'+'20:17:2.5'
+        currentTime = datetime.strptime(Str, "%Y-%m-%dT%H:%M:%S.%f")
+
+    else:
+        # print('No positioning')
+        GPS=False
+        currentTime=datetime.now()
+
+    print(currentTime)
+    os.system("sudo echo Using GPS "+ str(GPS)+' Time '+ str(currentTime) + " >> /home/pi/Documents/debugger.txt 2>&1")
+    return currentTime,GPS
+
+
+
 os.system("sudo echo IRan! >> /home/pi/Documents/debugger.txt 2>&1")
 # try:
 x = L76X.L76X()
@@ -36,6 +65,8 @@ AAlt = 0.000
 # Read the times from the input text file.
 Date=[]
 Doppler=[]
+Length=[]
+sampleRate=2000000
 
 #IMPORTANT: Cannot have extra white space at end of InputTimes.txt. It will throw "index out of range" error
 fileName='InputTimes.txt'
@@ -49,60 +80,24 @@ with open('/home/pi/GPS/Unification_Attempt/'+fileName) as f:
         h1=datetime.strptime(Str[0],"%Y-%m-%dT%H:%M:%S.%f")
         Date.append(h1)
         Doppler.append(float(Str[1]))
+        Length.append(float(Str[2]))
         print("Before Schedule utc: " + str(h1))
         os.system("sudo echo "+"Before Schedule utc: " + str(h1)+" >> /home/pi/Documents/debugger.txt 2>&1")
 
 i=0
+currentTime,GPS=getCurrentTime(x)
+while i<len(Date):
+    if currentTime>=Date[i]:
+        i=i+1
+    else:
+        break
+
+
+
 while (i<len(Date)):
 
-    try:
-        x.L76X_Get()
-    except:
-        os.system("sudo echo Failed To Get GPS >> /home/pi/Documents/debugger.txt 2>&1")
-        x.Status="0"
-    # print('\n')
-
-    #Get the current time to the best of our ability.
-    if (x.Status == "1"):
-        GPS=True;
-        # 2020-3-1
-        # 20:17:2.5
-        Str=x.Date+'T'+x.Time
-        # Str='2020-3-1'+'T'+'20:17:2.5'
-        currentTime = datetime.strptime(Str, "%Y-%m-%dT%H:%M:%S.%f")
-
-        # currentTime=datetime.now()
-
-        # print('Position aquired')
-
-        # print("Date: " + x.Date)
-        # print("Time: " + x.Time)
-        # print('Lat = %f' % x.Lat)
-        # print('Lon = %f' % x.Lon)
-        # print('Alt = %f' % x.Alt)
-
-        # ALat = (ALat * (1 - (1 / i))) + (x.Lat * (1 / i))
-        # ALon = (ALon * (1 - (1 / i))) + (x.Lon * (1 / i))
-        # AAlt = (AAlt * (1 - (1 / i))) + (x.Alt * (1 / i))
-
-        # print('ALat = %f' % ALat)
-        # print('ALon = %f' % ALon)
-        # print('AAlt = %f' % AAlt)
-
-        # os.system('sudo timedatectl set-time \'' + x.Date + ' ' + x.Time + '\'')
-        # f.write(
-        #     '%f' % x.Lat + ' %f' % x.Lon + ' %f' % x.Alt + ' %0.f:' % x.Time_H + '%0.f:' % x.Time_M + '%.2f' % x.Time_S + '\n')
-
-        # i = i + 1
-
-    else:
-        # print('No positioning')
-        GPS=False
-        currentTime=datetime.now()
-
+    currentTime, GPS = getCurrentTime(x)
     # Compare current time and the set times.
-    print(currentTime)
-    os.system("sudo echo Using GPS "+ str(GPS)+' Time '+ str(currentTime) + " >> /home/pi/Documents/debugger.txt 2>&1")
     if currentTime>=Date[i]:
         # print('Scheduled Time: '+str(Date[i]))
 
@@ -114,12 +109,12 @@ while (i<len(Date)):
         # String='python /home/pi/GIT_GNU/GNU/GNU_code/Record_ref/record_ref.py --channel-freq='+'162400000'+' --samp-rate=2000000 --center-freq=162000000 --num-samples=10000000 --file-loc="/home/pi/Documents/Time'+str(datetime.now()).replace(" ","_").replace(":","_").replace(".","_")+'"'
         # String='python /home/pi/GIT_GNU/GNU/GNU_code/Record_ref/record_ref.py --channel-freq='+'97900000'+' --samp-rate=2000000 --center-freq=97000000 --num-samples=10000000 --file-loc="/home/pi/Documents/Time'+str(datetime.now()).replace(" ","_").replace(":","_").replace(".","_")+'"'
         if Doppler[i] > 0:
-            String='python /home/pi/GIT_GNU/GNU/GNU_code/Record_ref/record_ref.py --channel-freq='+str(int(round(Doppler[i]*1e6)))+' --samp-rate=2000000 --center-freq=437000000 --num-samples=1800000000 --file-loc="/home/pi/Documents/Time'+str(currentTime).replace(" ","_").replace(":","_").replace(".","_")+'"'
+            String='python /home/pi/GIT_GNU/GNU/GNU_code/Record_ref/record_ref.py --channel-freq='+str(int(round(Doppler[i]*1e6)))+' --samp-rate='+str(sampleRate)+' --center-freq=437000000 --num-samples='+str(int(round(Length[i]*sampleRate)))+' --file-loc="/home/pi/Documents/Ref_Time'+str(currentTime).replace(" ","_").replace(":","_").replace(".","_")+'"'
             # String = 'python /home/pi/GIT_GNU/GNU/GNU_code/Record_ref/record_ref.py --channel-freq=' + '437200000' + ' --samp-rate=2000000 --center-freq=437000000 --num-samples=12000000 --file-loc="/home/pi/Documents/Time' + str(
             #     currentTime).replace(" ", "_").replace(":", "_").replace(".", "_") + '"'
         else:
             # String='python /home/pi/GIT_GNU/GNU/GNU_code/Record_ref/record_ref.py --channel-freq='+'162400000'+' --samp-rate=2000000 --center-freq=162000000 --num-samples=10000000 --file-loc="/home/pi/Documents/Time'+str(datetime.now()).replace(" ","_").replace(":","_").replace(".","_")+'"'
-            String = 'python /home/pi/GIT_GNU/GNU/GNU_code/Record_ref/record_ref.py --channel-freq=' + '97900000' + ' --samp-rate=2000000 --center-freq=97000000 --num-samples=10000000 --file-loc="/home/pi/Documents/Time' + str(
+            String = 'python /home/pi/GIT_GNU/GNU/GNU_code/Record_ref/record_ref.py --channel-freq=' + '97900000' + ' --samp-rate='+str(sampleRate)+' --center-freq=97000000 --num-samples='+str(int(round(Length[i]*sampleRate)))+' --file-loc="/home/pi/Documents/Sat_Time' + str(
                 currentTime).replace(" ", "_").replace(":", "_").replace(".", "_") + '"'
 
         # time.sleep(10)
@@ -147,6 +142,9 @@ while (i<len(Date)):
 
 os.system("sudo echo All Scheduled Times Completed >> /home/pi/Documents/debugger.txt 2>&1")
 print("All scheduled Times Completed")
+
+
+
 
 
 
